7be461c9f6814c2cc9e00d73367046310ba8bbfe
João Felipe Soares <joaofsoares@yahoo.com>
2025-05-13 10:26:46 +0100

    Create pascal.scala

---
+++ src/main/scala/misc/pascal.scala
@@ -0,0 +1,20 @@
+object PascalsTriangle {
+  def rows(row: Int): List[List[Int]] = {
+    if row <= 0 then List()
+    else {
+      var triangle: List[List[Int]] = List.empty[List[Int]]
+
+      for i <- 0 until row do {
+
+        val arr = (0 to i).map(j => {
+          if i == j || j == 0 then 1 else triangle(i - 1)(j - 1) + triangle(i - 1)(j)
+        }).toList
+
+        triangle = triangle :+ arr
+
+      }
+
+      triangle
+    }
+  }
+}
