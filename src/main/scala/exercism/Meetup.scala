8907824d7f8f4f73399ed7a37ced3024256cdd65
joaofsoares <joaofsoares@yahoo.com>
2026-01-06 11:36:28 +0000

    meetup, maybe performance issue

--- src/main/scala/exercism/Meetup.scala
+++ src/main/scala/exercism/Meetup.scala
@@ -1,73 +1,33 @@
 import java.time.{YearMonth, DayOfWeek, LocalDate}
-import java.time.temporal.ChronoUnit
 
 import Schedule.Schedule
 
 case class Meetup(month: Int, year: Int) {
   def day(dayOfWeek: Int, schedule: Schedule): LocalDate = {
+
     val monthSize = YearMonth.of(year, month).lengthOfMonth
 
-    // val start = LocalDate.of(year, month, 1)
-    // val end = LocalDate.of(year, month, monthSize)
+    val days = if (schedule == Schedule.Teenth) {
+      (13 to 19).filter(f => {
+        val localDate = LocalDate.of(year, month, f)
+        localDate.getDayOfWeek.getValue == dayOfWeek
+      })
 
-    // val weeks = ChronoUnit.WEEKS.between(start, end)
-
-    // println(s"weeks = $weeks")
-
-    // val mondays = (1 to monthSize)
-    // .filter(i => {
-    // val localDate = LocalDate.of(year, month, i)
-    // localDate.getDayOfWeek.getValue == dayOfWeek
-    // })
-
-    var result: LocalDate = null
-
-    schedule match {
-      case Schedule.First =>
-        for i <- 1 to 7 do {
-          val localDate = LocalDate.of(year, month, i)
-          if (localDate.getDayOfWeek.getValue == dayOfWeek) {
-            result = localDate
-          }
-        }
-      case Schedule.Second =>
-        for i <- 8 to 15 do {
-          val localDate = LocalDate.of(year, month, i)
-          if (localDate.getDayOfWeek.getValue == dayOfWeek) {
-            result = localDate
-          }
-        }
-      case Schedule.Third =>
-        for i <- 15 to 22 do {
-          val localDate = LocalDate.of(year, month, i)
-          if (localDate.getDayOfWeek.getValue == dayOfWeek) {
-            result = localDate
-          }
-        }
-      case Schedule.Fourth =>
-        for i <- 22 to 29 do {
-          val localDate = LocalDate.of(year, month, i)
-          if (localDate.getDayOfWeek.getValue == dayOfWeek) {
-            result = localDate
-          }
-        }
-      case Schedule.Last =>
-        for i <- 29 to monthSize do {
-          val localDate = LocalDate.of(year, month, i)
-          if (localDate.getDayOfWeek.getValue == dayOfWeek) {
-            result = localDate
-          }
-        }
-      case Schedule.Teenth =>
-        for i <- 13 to 19 do {
-          val localDate = LocalDate.of(year, month, i)
-          if (localDate.getDayOfWeek.getValue == dayOfWeek) {
-            result = localDate
-          }
-        }
+    } else {
+      (1 to monthSize).filter(d => {
+        val localDate = LocalDate.of(year, month, d)
+        localDate.getDayOfWeek.getValue == dayOfWeek
+      })
     }
 
-    result
+    return schedule match {
+      case Schedule.First  => LocalDate.of(year, month, days.head)
+      case Schedule.Second => LocalDate.of(year, month, days.tail.head)
+      case Schedule.Third  => LocalDate.of(year, month, days(2))
+      case Schedule.Fourth => LocalDate.of(year, month, days(3))
+      case Schedule.Last   => LocalDate.of(year, month, days.last)
+      case Schedule.Teenth => LocalDate.of(year, month, days.head)
+    }
   }
 }
 
